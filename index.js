27784af1046b2f58e2d8d0511ff71bc155f612dc
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-09-12 20:42:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const express = require('express');
-const { Telegraf } = require('telegraf');
-const axios = require('axios');
 
-
-require('dotenv').config();
-
-
-//Creacion de la app con express
-const app = express();
-const bot = new Telegraf(process.env.BOT_TOKEN);
 
